4d54b72f509750cd18357d53162a7f1ee5c996f1
Nico <nicolasaraya@gmail.com>
2023-02-27 18:14:53 -0300

    spec 11b and 11c done

--- spec/11c_dog_spec.rb
+++ spec/11c_dog_spec.rb
@@ -35,17 +35,18 @@ end
 
 describe Dog do
   # Create a subject with your choice of dog name and optional breed/color.
-
+  subject(:my_dog) { described_class.new('Tepa', 'Bernese mountain dog', 'Black') }
   # Write a test using the second shared_example to test that dog responds to
   # talk ('WOOF!').
-  context '' do
+  context 'when dog responds to talk' do
+    include_examples 'shared method name'
   end
 
-  # remove the 'x' before running this test
-  xit 'is not barking' do
+  it 'is not barking' do
+    expect(my_dog).not_to be_barking
   end
 
-  # remove the 'x' before running this test
-  xit 'is sleeping' do
+  it 'is sleeping' do
+    expect(my_dog).to be_sleeping
   end
 end
